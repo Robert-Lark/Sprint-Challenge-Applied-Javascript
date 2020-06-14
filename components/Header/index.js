bcb3869fa8254053568825bf8f6df5225617b469
Rob Lark <quietcalmrecords@Robs-iMac.local>
2020-06-14 16:25:13 -0700

    completed header

--- components/Header/index.js
+++ components/Header/index.js
@@ -11,4 +11,24 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+function Header() {
+
+    ////create the elements
+    const newDiv = document.createElement('div');
+    const newSpan = document.createElement('span');
+    const newH1 = document.createElement('h1');
+    const newSpan1 = document.createElement('span');
+    //add styling
+    newDiv.classList.add('header');
+    newSpan.classList.add('date');
+    newSpan1.classList.add('temp');
+    newSpan.innerText = 'SMARCH 28, 2019';
+    newH1.innerText = 'Lambda Times';
+    newSpan1.innerText='98°';
+    //put it on the page
+    const headerContainer = document.querySelector('.header-container');
+    headerContainer.appendChild(newDiv);
+
+}
+
+Header();
