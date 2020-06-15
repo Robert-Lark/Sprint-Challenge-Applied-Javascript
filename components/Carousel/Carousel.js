85275e7f4120dc39905dd665e29edce1540abe79
Rob Lark <quietcalmrecords@Robs-iMac.local>
2020-06-15 14:14:57 -0700

    third commit

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -19,49 +19,30 @@
 */
 
 const carousel = document.createElement('div');
-carousel.classList.add = ('carousel');
-
-
+carousel.classList.add('carousel');
 
   function bambamComponant(image){
-      //crete the div the image will live in
-      // const imgDiv = document.createElement('div');
-      //give it a class
-      // imgDiv.classList.add('carouselImgDiv');
-      //create the image
       const carouselImg = document.createElement('img');
-      // set its source
       carouselImg.src = image;
       carouselImg.style = 'width: 440px; height: 250px;'
-      //append it to its div
-      // imgDiv.appendChild(carouselImg);
-      //give it a class
       carouselImg.classList.add('carouselImg');
-    
       return carouselImg
     }
   
-
   const imgDiv1 = bambamComponant("assets/carousel/Screen Shot 2020-06-08 at 1.40.56 PM.png");
   const imgDiv2 = bambamComponant("assets/carousel/Screen Shot 2020-06-08 at 1.41.27 PM.png");
   const imgDiv3 = bambamComponant("assets/carousel/Screen Shot 2020-06-08 at 1.41.48 PM.png");
   const imgDiv4 = bambamComponant("assets/carousel/Screen Shot 2020-06-08 at 1.41.17 PM.png");
 
-  
-
   const carouselContainer = document.querySelector('.carousel-container');
-
   carouselContainer.appendChild(carousel);
-  
   const carouselbuttonLeft = document.createElement('div');
   carouselbuttonLeft.classList.add('left-button');
   carousel.appendChild(carouselbuttonLeft);
-
   carousel.appendChild(imgDiv1);
   carousel.appendChild(imgDiv2);
   carousel.appendChild(imgDiv3);
   carousel.appendChild(imgDiv4);
-
   const carouselbuttonRight = document.createElement('div');
   carouselbuttonRight.classList.add('right-button');
   carousel.appendChild(carouselbuttonRight);
@@ -73,40 +54,3 @@ carousel.classList.add = ('carousel');
 
 
 
-// // carouselbuttonLeft.addEventListener('click', function(){
-// //   const slides = document.querySelectorAll('.carouselImg');
-// //    let slideIndex = Array.from(slides);
-// // let slideArray = [];
-// // console.log(slideArray);
-// //    slideIndex.forEach((img) => {
-// //      slideArray.push(img);
-// //    })
-//   // slideIndex.push(slides[0]);
-//   //   slideIndex.push(slides[1]);
-//   //   slideIndex.push(slides[2]);
-//   //   slideIndex.push(slides[3]);
-
-    
-
-//     // console.log(slide);
-
-// // })
-
-// //   let slideIndex = slides[0];
-  
-// //   function showSlides(n) {
-   
-// //     
-// //     if (n > 4) {
-// //         slideIndex = 1 
-// //     }
-// //     if (n < 1) {
-// //         slideIndex = 3
-// //     }
-// //     slides[0].style.display = "none";
-// //     slides[1].style.display = "none";
-// //     slides[2].style.display = "none";
-// //     slides[3].style.display = "none";
-// // //     slides[slideIndex - 1].style.display = "block";
-// // }
-// // showSlides(slideIndex);
